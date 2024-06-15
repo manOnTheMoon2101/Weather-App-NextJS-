130dc5e6dd178ecba46d9fb1c047992350ee2c6e
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-06-15 13:01:30 +0200

    updated frontend to look more modern and simple(also fixed view on mobile).

--- app/components/WeatherData/weatherData.tsx
+++ app/components/WeatherData/weatherData.tsx
@@ -3,30 +3,40 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Noto_Sans_Batak } from "next/font/google";
-import useSWR from "swr";
+import { Averia_Sans_Libre } from "next/font/google";
+import { CiSearch } from "react-icons/ci";
 import Description from "../Description/Description";
 import styles from "../../page.module.css";
-import Spline from "@splinetool/react-spline";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const noto = Noto_Sans_Batak({
   subsets: ["latin"],
   weight: "400",
 });
-
+const averia = Averia_Sans_Libre({
+  subsets: ["latin"],
+  weight: "400",
+});
 function WeatherData() {
   const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState(false);
   //const fetcher = (url: string) => fetch(url).then((res) => res.json());
-
+  // `https://weather-app-next-js-pi.vercel.app/api/weather/${city}`
   //const { data } = useSWR(`http://localhost:3000/api/weather/paarl`, fetcher);
   const fetchData = async () => {
+    setIsLoading(true);
     try {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
       const response = await axios.get(
-        `https://weather-app-next-js-pi.vercel.app/api/weather/${city}`
+        `http://localhost:3000/api/weather/${city}`
       );
       setWeatherData(response.data);
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   };
   const currentDate = new Date(Date.now());
@@ -55,35 +65,42 @@ function WeatherData() {
             className={styles.searchI}
             onChange={handleInputChange}
           />
-          <button className={styles.submitB} type="submit">
-            Search
+          <button className={styles.icon} type="submit">
+            <CiSearch />
           </button>
         </div>
       </form>
-
+      {isLoading ? (
+        <div className={styles.skeletonDiv}>
+          <SkeletonTheme baseColor="#45474B" highlightColor="#F4CE14">
+            <Skeleton className={styles.skeletonLine} count={1} width={200} />
+          </SkeletonTheme>
+        </div>
+      ) : (
+        <div></div>
+      )}
       {weatherData ? (
         <>
-          <h2 style={{ textAlign: "center", fontWeight: "600",color:"#495E57" }}>
+          <h2
+            style={{ textAlign: "center", fontWeight: "600", color: "#495E57" }}
+          >
             {weatherData.name}
           </h2>
 
-       
-            <Description value={weatherData.weather[0].description} />
-            
-     
+          <Description value={weatherData.weather[0].description} />
 
           <div style={{ margin: "20px" }}>
             <p style={{ fontSize: "30px", color: "black" }}>
               {weatherData.main.temp}°C
             </p>
             <p
-              className={noto.className}
+              className={averia.className}
               style={{ fontSize: "20px", color: "grey" }}
             >
               {weatherData.weather[0].description}
             </p>
             <p
-              className={noto.className}
+              className={averia.className}
               style={{ fontSize: "20px", color: "rgb(244, 206, 20)" }}
             >
               {isoString}
@@ -91,7 +108,23 @@ function WeatherData() {
           </div>
         </>
       ) : (
-        <p style={{ textAlign: "center" }}>Loading weather data...</p>
+        <div className={styles.welcomeDiv}>
+          <h2
+            style={{ textAlign: "center", fontSize: "60px" }}
+            className={averia.className}
+          >
+            Welcome
+            <span>👋</span>
+          </h2>
+          {/* className={styles.emoji} animations for emoji */}
+          <p
+            style={{ textAlign: "center", color: "gray" }}
+            className={averia.className}
+          >
+            Enter you designated city in <br />
+            the search bar on top.
+          </p>
+        </div>
       )}
     </div>
   );
